fbcaaefa12e97e8eeac798ab638c8ab3dbff742c
santiag0tech <165143365+santiag0tech@users.noreply.github.com>
2024-04-16 10:45:50 +0200

    Update netplan_configuration.sh

--- networking_scripts/netplan_configuration.sh
+++ networking_scripts/netplan_configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the user has superuser privileges
-if [ `id -u` != "0" ]; then
+if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
@@ -30,31 +30,59 @@ configure_network_interface() {
             ;;
         *)
             echo "Invalid option."
+            return
             ;;
     esac
 
-    # Generate the Netplan configuration
-    netplan_config="\
+    # Check if Netplan configuration file exists
+    netplan_file="/etc/netplan/01-network-manager-all.yaml"
+    if [ -f "$netplan_file" ]; then
+        # Load existing configuration
+        current_config=$(<"$netplan_file")
+    else
+        current_config="\
 network:
   version: 2
   renderer: networkd
-  ethernets:
-    $interface:
-      dhcp4: $dhcp
 "
+    fi
+
+    # Check if ethernets section already exists
+    if [[ ! "$current_config" =~ ethernets ]]; then
+        ethernets_section="\
+  
+  ethernets:
+"
+    else
+        ethernets_section=""
+    fi
+
+    # Generate the Netplan configuration
+    new_config=""
 
     # Add additional configuration if not using DHCP
     if [ "$dhcp" = false ]; then
-        netplan_config+="\
+        new_config+="\
+$ethernets_section
+    $interface:
       addresses: [$ip_address/$subnet_mask]
       gateway4: $gateway
       nameservers:
         addresses: [$dns_servers]
 "
+    else
+        new_config+="\
+$ethernets_section
+    $interface:
+      dhcp4: true
+"
     fi
 
-    # Save the configuration to the Netplan configuration file
-    echo "$netplan_config" > /etc/netplan/01-network-manager-all.yaml
+    # Combine existing and new configurations
+    combined_config="$current_config$new_config"
+
+    # Save the combined configuration to the Netplan configuration file
+    echo "$combined_config" > "$netplan_file"
 
     # Apply the Netplan configuration
     netplan apply
